869871bf6198b3f30ce3255b26dec5abd8cd504a
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-26 20:39:42 +0900

    バリデーション

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,14 @@
 class Review < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :summary
+    validates :contents
+  end
+
+  validates :points_id, numericality: { other_than: 1 }
+
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+
 end
