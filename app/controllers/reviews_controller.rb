3815112787a3d1f5362f00df956a7b0a3e6692a8
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-26 21:11:02 +0900

    createアクションとストロングパラメーター

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,16 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :points_id, :summary, :contents).merge(user_id: current_user.id)
   end
 
 end
