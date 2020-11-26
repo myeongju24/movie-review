6c004100fb10657046695e4dbbd4698fc362ad27
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-27 03:13:10 +0900

    Update point.rb

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,16 +1,16 @@
 class Point < ActiveHash::Base
   self.data = [
     { id: 1,  name: '--' },
-    { id: 2,  name: '1'},
-    { id: 3,  name: '2' },
-    { id: 4,  name: '3' },
-    { id: 5,  name: '4' },
-    { id: 6,  name: '5' },
-    { id: 7,  name: '6' },
-    { id: 8,  name: '7' },
-    { id: 9,  name: '8' },
-    { id: 10, name: '9' },
-    { id: 11, name: '10' }
+    { id: 2,  name: '1点'},
+    { id: 3,  name: '2点' },
+    { id: 4,  name: '3点' },
+    { id: 5,  name: '4点' },
+    { id: 6,  name: '5点' },
+    { id: 7,  name: '6点' },
+    { id: 8,  name: '7点' },
+    { id: 9,  name: '8点' },
+    { id: 10, name: '9点' },
+    { id: 11, name: '10点' }
   ]
 
   include ActiveHash::Associations
