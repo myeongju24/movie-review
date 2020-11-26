4f5877dc9af13d182a9e870b1d38fc21a34c05cd
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-27 04:48:37 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "reviews#index"
-  resources :reviews, only: [:new, :create, :show, :destroy, :edit, :update] do
+  resources :reviews, except: [:index] do
     resources :comments, only: :create
   end
   resources :users ,only: :show
