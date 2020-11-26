86b063c18f3fb58269660f6e47e5804f23babf1d
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-26 18:44:54 +0900

    マイグレーションの修正
    favoritemovieにアクティブハッシュを導入するためにマイグレーションファイルを変更

--- db/migrate/20201123221121_devise_create_users.rb
+++ db/migrate/20201123221121_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string  :email,              null: false, default: ""
       t.string  :encrypted_password, null: false, default: ""
       t.string  :favorite_movie,     null: false
-      t.integer :favorite_genre,     null: false
+      t.integer :favorite_genre_id,     null: false
       
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_221121) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "favorite_movie", null: false
-    t.integer "favorite_genre", null: false
+    t.integer "favorite_genre_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
