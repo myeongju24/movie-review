bbac82352f75126bfc2641bd89e48a52fca9aa95
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-27 02:37:13 +0900

    commentアクションの中身

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to review_path(@comment.review)
+    else
+      @review = @comment.review
+      @comments = @review.comments
+      render "reviews/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, review_id: params[:review_id])
+  end
 end
