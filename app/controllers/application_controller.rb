9a1fa46ea6d4173df129cd47aa9a6cb273e610a0
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-26 19:19:56 +0900

    ユーザー登録のストロングパラメーター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  private
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :favorite_movie, :favorite_genre_id])
+end
 end
